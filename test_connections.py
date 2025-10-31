6cf3b41df6324dc902f1ce361b0931f1573aef1e
Vishal Gangurde <gangurdev1910@gmail.com>
2025-10-31 11:51:50 +0530

    Test-Commit

---
+++ test_connections.py
@@ -0,0 +1,8 @@
+def greet_team():
+    team_members = ["Vishal", "Sandy"]
+    for member in team_members:
+        print(f"Hello {member}, 👋 Welcome to the Real-Time Chatbot Agent project.")
+
+if __name__ == "__main__":
+    greet_team()
+    print("Successfully connected to the github repository.")
